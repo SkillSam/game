82ec0ee4cdb25a0bb28941c3842cec7ec1dad1c7
jill-kapchan <jkapchan@asu.edu>
2018-11-09 23:12:47 -0700

    h

--- src/main/java/me/skillsam/Main.java
+++ src/main/java/me/skillsam/Main.java
@@ -5,7 +5,7 @@ public class Main {
 	public static void main(String[] args) {
 		System.out.println("Hello world!");
 		System.out.print("HI");
-		System.out.print("Jill");
+		System.out.print("J");
 	}
 	
 }
