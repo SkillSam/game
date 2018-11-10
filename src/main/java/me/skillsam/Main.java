42ac069feee9e2a55b3a37afdc600d4c788581aa
SkillSam <SkillSam98@gmail.com>
2018-11-09 23:24:24 -0700

    hai

--- src/main/java/me/skillsam/Main.java
+++ src/main/java/me/skillsam/Main.java
@@ -3,11 +3,14 @@ package me.skillsam;
 public class Main {
 	
 	public static void main(String[] args) {
-		int counter = 0;
+		int counter = 0, sum = 0;
 		
-		for (int i = 0; i < 10; i++) counter++;
+		for (int i = 0; i < 10; i++) {
+			counter++;
+			sum += i;
+		}
 		
-		System.out.println(counter);
+		System.out.println(counter + " - " + sum);
 		
 		System.out.println("Hello world!");
 		System.out.print("HI");
