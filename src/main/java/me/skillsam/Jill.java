30da7a339048912efe8388258ada4ae861a2c8a3
jill-kapchan <jkapchan@asu.edu>
2018-11-10 11:35:28 -0700

    pix

---
+++ src/main/java/me/skillsam/Jill.java
@@ -0,0 +1,22 @@
+package me.skillsam;
+
+public class Jill extends BasicGame {
+	private Image arm1_1 = null;
+	private Image arm1_2 = null;
+	private Image arm2_1 = null;
+	private Image arm2_2 = null;
+	private Image arm3_1 = null;
+	private Image arm3_2 = null;
+	private Image arm4_1 = null;
+	private Image arm4_2 = null;
+	
+	public SetupClass(String title)
+	{
+		super(title);
+	}
+	
+	public void init(GameContainer container) throw SlickException {
+		arm1_1 = new Image("src/main/resources")
+	}
+	
+}
