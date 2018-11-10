a279c0ae182c341050e6f8455f2207d056412fae
SkillSam <SkillSam98@gmail.com>
2018-11-10 14:13:53 -0700

    Reset

--- src/main/java/me/skillsam/Main.java
+++ src/main/java/me/skillsam/Main.java
@@ -1,43 +1,18 @@
 package me.skillsam;
 
-import org.lwjgl.LWJGLException;
-import org.lwjgl.opengl.Display;
-import org.lwjgl.opengl.DisplayMode;
+import javafx.application.Application;
+import javafx.stage.Stage;
 
-public class Main {
-	
-	public void start() {
-		try {
-			Display.setDisplayMode(new DisplayMode(800, 600));
-			Display.create();
-		} catch (LWJGLException e) {
-			e.printStackTrace();
-		}
-		
-		while (!Display.isCloseRequested()) {
-			Display.update();
-		}
-		
-		Display.destroy();
-	}
+public class Main extends Application {
 	
 	public static void main(String[] args) {
+		launch(args);
+	}
+
+	@Override
+	public void start(Stage primaryStage) throws Exception {
+		// TODO Auto-generated method stub
 		
-		new Main().start();
-		
-		int counter = 0, sum = 0;
-		
-		for (int i = 0; i < 10; i++) {
-			counter++;
-			sum += i;
-		}
-		
-		System.out.println(counter + " - " + sum);
-		
-		System.out.println("Hello world!");
-		System.out.print("HI");
-		System.out.print("Crystal");
-		System.out.print("Jill");
 	}
 	
 }
