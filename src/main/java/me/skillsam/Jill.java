929fafaf102c0470616c57f243ab9cc124e340bf
jill-kapchan <jkapchan@asu.edu>
2018-11-10 11:57:43 -0700

    update

--- src/main/java/me/skillsam/Jill.java
+++ src/main/java/me/skillsam/Jill.java
@@ -1,6 +1,9 @@
 package me.skillsam;
 
+//import javafx.scene.image.Image;
+
 public class Jill extends BasicGame {
+	
 	private Image arm1_1 = null;
 	private Image arm1_2 = null;
 	private Image arm2_1 = null;
@@ -15,8 +18,21 @@ public class Jill extends BasicGame {
 		super(title);
 	}
 	
-	public void init(GameContainer container) throw SlickException {
-		arm1_1 = new Image("src/main/resources")
+	public void init(GameContainer container) throws SlickException {
+		arm1_1 = new Image("src/main/resources/arm1_1.png");
 	}
 	
+	public void render(GameContainer container, Graphics g) throws SlickException {
+		g.drawString("Hello World!, 20, 20");
+		arm1_1.draw();
+	}
+	
+	public static void(String[] args) throws SlickException {
+		AppContainer app = new AppGameContainer(new SetupClass("Setup test"));
+		
+		app.setDisplayMode(800, 600, false);
+		app.setAlwaysRender(true);
+		
+		app.start();
+	}
 }
