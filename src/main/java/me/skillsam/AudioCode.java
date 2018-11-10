375c0c93b418b5a691adafdd26775048b2597076
jill-kapchan <jkapchan@asu.edu>
2018-11-10 14:52:51 -0700

    audio

---
+++ src/main/java/me/skillsam/AudioCode.java
@@ -0,0 +1,43 @@
+package me.skillsam;
+import java.io.File;
+
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.layout.StackPane;
+import javafx.scene.media.Media;
+import javafx.scene.media.MediaPlayer;
+import javafx.stage.Stage;
+
+
+public class AudioCode extends Application{
+	private MediaPlayer mediaPlayer;
+	
+	public void start(Stage primaryStage) {
+		
+ 		StackPane root = new StackPane();
+		
+ 		String sound = "src/main/resources/audio.mp3";
+ 		Media file = new Media(new File(sound).toURI().toString());
+		mediaPlayer = new MediaPlayer(file);
+		mediaPlayer.setAutoPlay(true);
+		
+		Runnable onEnd = new Runnable() {
+			public void run() {
+				mediaPlayer.dispose();
+				mediaPlayer = new MediaPlayer(file);
+				mediaPlayer.play();
+				mediaPlayer.setOnEndOfMedia(this);
+			}};
+		mediaPlayer.setOnEndOfMedia(onEnd);
+		mediaPlayer.play();
+			
+
+		primaryStage.setScene(new Scene(root, 800, 600));
+        primaryStage.show();
+	}
+	
+    public static void main(String[] args)
+    {
+        launch(args);
+    }
+}
