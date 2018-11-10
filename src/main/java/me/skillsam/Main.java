f9d8d56e45e6be357058a3724f78c901497965dd
crystal-nguyen <cnguye30@asu.edu>
2018-11-10 14:54:04 -0700

    moved arm transition class into buttons

--- src/main/java/me/skillsam/Main.java
+++ src/main/java/me/skillsam/Main.java
@@ -2,17 +2,28 @@ package me.skillsam;
 
 import javafx.application.Application;
 import javafx.stage.Stage;
+import javafx.scene.Scene;
+import javafx.scene.layout.StackPane;
 
-public class Main extends Application {
-	
-	public static void main(String[] args) {
-		launch(args);
-	}
-
-	@Override
-	public void start(Stage primaryStage) throws Exception {
-		// TODO Auto-generated method stub
-		
-	}
-	
+public class Main extends Application
+{
+   public void start(Stage primaryStage)
+   {   
+      //create a DrawPane object. See DrawPane.java for details.
+      Buttons gui = new Buttons();
+   
+      //put gui on top of the rootPane
+      StackPane rootPane = new StackPane();
+      rootPane.getChildren().add(gui);
+   
+      // Create a scene and place rootPane in the stage
+      Scene scene = new Scene(rootPane, 600, 400);
+      primaryStage.setTitle("Shape Drawing"); 
+      primaryStage.setScene(scene); // Place the scene in the stage
+      primaryStage.show(); // Display the stage
+   }
+   public static void main(String[] args)
+   {
+      Application.launch(args);
+   }
 }
